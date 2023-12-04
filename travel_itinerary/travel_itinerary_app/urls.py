f95567b586a9608d8c2557ab19246fec8dc9e3d9
HaoLanLi <99941218+HaoLanLi@users.noreply.github.com>
2023-12-04 17:39:10 -0500

    Update urls.py

--- travel_itinerary/travel_itinerary_app/urls.py
+++ travel_itinerary/travel_itinerary_app/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('itinerary/', views.itinerary, name='itinerary'),
     path('profile/', views.profile, name='profile'),
+    path('ratings/', views.ratings, name='ratings'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('itinerary_form/', views.itinerary_form, name='itinerary_form'),
     path("search_results/", views.search_results, name="search_results"),
